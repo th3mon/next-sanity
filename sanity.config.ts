64c4937064d9d40ec6a97efc7fc3c2e0d692568e
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-10 03:08:56 +0200

    Add Project Schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import project from '@/sanity/schemas/project.schema';
 
 const config = defineConfig({
   projectId: 'dydfr307',
@@ -7,7 +8,8 @@ const config = defineConfig({
   title: 'My Personal Website',
   apiVersion: '2023-04-10',
   baseUrl: '/admin',
-  plugins: [deskTool()]
+  plugins: [deskTool()],
+  schema: { types: [project] }
 });
 
 export default config;
