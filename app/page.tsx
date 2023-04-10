30740b66eece1cd5329a74334b6acb57a3f8ff0d
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-10 20:09:09 +0200

    Link listed projects on Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { getProjects } from '@/sanity/sanity-utils';
 
 export default async function Home() {
@@ -17,7 +18,11 @@ export default async function Home() {
       </h2>
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <div key={project._id} className="border-2 border-gray-500 rounded-lg p-1">
+          <Link
+            key={project._id}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
+            href={`/projects/${project.slug}`}
+          >
             {
               project.image && (
                 <Image
@@ -32,7 +37,7 @@ export default async function Home() {
             <div className="mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-rose-600 to-purple-600 bg-clip-text text-transparent">
               {project.name}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
