048b43d4a927aed7e6268edbc255b67eae21edf1
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-10 03:03:02 +0200

    Fix indentations

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 
 const config = defineConfig({
-    projectId: 'dydfr307',
-    dataset: 'production',
-    title: 'My Personal Website',
-    apiVersion: '2023-04-10',
-    baseUrl: '/admin',
-    plugins: [deskTool()]
+  projectId: 'dydfr307',
+  dataset: 'production',
+  title: 'My Personal Website',
+  apiVersion: '2023-04-10',
+  baseUrl: '/admin',
+  plugins: [deskTool()]
 });
 
 export default config;
