2c0358de3dd22fe7c53b296a0a9d365081bb7533
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-10 22:00:27 +0200

    Add header on the Project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -7,5 +7,22 @@ export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
 
-  return <div>{project.name}</div>;
+  return (
+    <div className="max-w-3xl mx-auto py-20">
+      <header className="flex items-center justify-between">
+        <h1 className="bg-gradient-to-r from-orange-400 via-rose-600 to-purple-600 bg-clip-text text-transparent text-5xl font-extrabold leading-normal">
+          {project.name}
+        </h1>
+        <a
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Project
+        </a>
+      </header>
+    </div>
+  );
 }
