583c6e54b37fc1123b015db40881911c3b333415
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-10 03:44:53 +0200

    Add Project type

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -2,8 +2,9 @@ import {
   createClient,
   groq
 } from 'next-sanity';
+import { Project } from '@/types/Project';
 
-export async function getProjects() {
+export async function getProjects(): Promise<Project[]> {
   const client = createClient({
     projectId: 'dydfr307',
     dataset: 'production',
@@ -20,5 +21,5 @@ export async function getProjects() {
       url,
       content
     }`
-  )
+  );
 }
