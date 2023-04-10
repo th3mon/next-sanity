2bfef32b2a21fc1c628239538b315019430724e2
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-10 21:39:17 +0200

    Add Project view page

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -23,3 +23,25 @@ export async function getProjects(): Promise<Project[]> {
     }`
   );
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: 'dydfr307',
+    dataset: 'production',
+    apiVersion: '2023-04-10',
+  });
+
+  // TODO: Find out if it is possible to get just one record
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
+  );
+}
