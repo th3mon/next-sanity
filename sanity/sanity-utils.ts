78e8273274802f36de241584b4e2f3d71c8dc755
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-11 02:19:45 +0200

    Remove TODO

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -21,7 +21,6 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  // TODO: Find out if it is possible to get just one record
   return createClient(clientConfig).fetch(
     groq`*[_type == "project" && slug.current == $slug][0]{
       _id,
