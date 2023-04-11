d12eb3f283fd0d55a95fcf9660d18064855a5cf3
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-11 02:14:42 +0200

    Install Vision Tool for groq

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'sanity';
+import { visionTool } from '@sanity/vision';
 import { deskTool } from 'sanity/desk';
 import schemas from '@/sanity/schemas';
 
@@ -8,7 +9,7 @@ const config = defineConfig({
   title: 'My Personal Website',
   apiVersion: '2023-04-10',
   baseUrl: '/admin',
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
   schema: { types: schemas }
 });
 
